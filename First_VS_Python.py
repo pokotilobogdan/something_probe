73900a745f4195f62ca7bfb20c8693ddec58437b
bohdanino <pokotilobogdan1998@gmail.com>
2024-03-12 19:19:48 +0100

    Function for printing text with delay

---
+++ First_VS_Python.py
@@ -0,0 +1,19 @@
+import time
+
+def print_delay(message, delay):
+    for char in message:
+        print(char, end = '')
+        time.sleep(delay)
+    print()
+
+
+if __name__ == "__main__":
+
+    message = "Shit, this message is written symbol-by-symbol. Damn, this is awesome!"
+    delay = 0.04
+
+    print_delay(message, delay)
+
+
+    print_delay(message, 0.05)
+    print_delay(message, 0.03)
